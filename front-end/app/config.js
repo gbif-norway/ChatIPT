1030e13a6b18627f6ea0461b40da0563693a57ae
Michal Torma <miso.torma.dev@gmail.com>
2025-07-08 01:15:45 +0200

    Add debug logging to config.js for improved visibility of base API URL and environment variable

--- front-end/app/config.js
+++ front-end/app/config.js
@@ -2,4 +2,8 @@ const config = {
   baseApiUrl: process.env.NEXT_PUBLIC_BASE_API_URL,
 };
 
+// Debug logging
+console.log('Config loaded with baseApiUrl:', config.baseApiUrl);
+console.log('Environment variable NEXT_PUBLIC_BASE_API_URL:', process.env.NEXT_PUBLIC_BASE_API_URL);
+
 export default config;
