b325c3620ed8ddaecf3ad1459176be8d37919a73
Michal Torma <miso.torma.dev@gmail.com>
2025-07-08 01:49:39 +0200

    Remove debug logging from config.js to clean up the code and improve performance.

--- front-end/app/config.js
+++ front-end/app/config.js
@@ -2,8 +2,4 @@ const config = {
   baseApiUrl: process.env.NEXT_PUBLIC_BASE_API_URL,
 };
 
-// Debug logging
-console.log('Config loaded with baseApiUrl:', config.baseApiUrl);
-console.log('Environment variable NEXT_PUBLIC_BASE_API_URL:', process.env.NEXT_PUBLIC_BASE_API_URL);
-
 export default config;
