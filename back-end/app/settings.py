91a0edbd67c4f068b20d93b6d1c1b4617a552632
rukayaj@gmail.com <rukayaj@gmail.com>
2025-08-04 15:00:40 +0200

    Fix sandbox for local only

--- back-end/app/settings.py
+++ back-end/app/settings.py
@@ -26,6 +26,9 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
+# ORCID environment selection: "production" or "sandbox", defaults to production
+ORCID_ENV = os.environ.get("ORCID_ENV", "production")
+
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1,0.0.0.0").split(",")
 
 # CORS_ALLOWED_ORIGINS = [
